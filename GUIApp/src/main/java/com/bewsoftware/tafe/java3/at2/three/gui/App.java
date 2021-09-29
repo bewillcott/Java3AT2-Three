ccec252c0a01c8d36e417adb9685d11019156d19
Bradley Willcott <bw.opensource@yahoo.com>
2021-09-30 00:53:52 +0800

    First successful attempt with FXML
    This code runs with no backing Controller yet.
    
    Also, now builds a fat-jar with all dependencies unpacked into it,
    excluding JRE.  The NewMain.java file is the solution to this working.

--- GUIApp/src/main/java/com/bewsoftware/tafe/java3/at2/three/gui/App.java
+++ GUIApp/src/main/java/com/bewsoftware/tafe/java3/at2/three/gui/App.java
@@ -1,38 +1,47 @@
+
 package com.bewsoftware.tafe.java3.at2.three.gui;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 /**
  * JavaFX App
  */
-public class App extends Application {
+public class App extends Application
+{
+    public App()
+    {
+        // NoOp
+    }
 
     private static Scene scene;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public static void main(String[] args)
+    {
+        launch();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
+    private static Parent loadFXML(String fxml) throws IOException
+    {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
-    public static void main(String[] args) {
-        launch();
+    static void setRoot(String fxml) throws IOException
+    {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException
+    {
+        scene = new Scene(loadFXML("MainWindow"), 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
--- GUIApp/src/main/java/module-info.java
+++ GUIApp/src/main/java/module-info.java
@@ -2,7 +2,9 @@ module GUI_Application {
     requires javafx.controls;
     requires javafx.fxml;
     requires CommonLibrary;
+    requires transitive javafx.graphics;
+    requires javafx.base;
+    requires java.base;
 
-    opens com.bewsoftware.tafe.java3.at2.three.gui to javafx.fxml;
-    exports com.bewsoftware.tafe.java3.at2.three.gui;
+    opens com.bewsoftware.tafe.java3.at2.three.gui to javafx.fxml, javafx.graphics;
 }
